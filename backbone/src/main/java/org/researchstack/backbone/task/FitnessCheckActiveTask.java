1b8e2430e3d3fdb039cea2ea55795883c5352353
suomi35 <jason@medable.com>
2019-07-07 13:25:26 -0600

    Updated FitnessCheckActiveTask with generalOutputDirectoy()

--- backbone/src/main/java/org/researchstack/backbone/task/FitnessCheckActiveTask.java
+++ backbone/src/main/java/org/researchstack/backbone/task/FitnessCheckActiveTask.java
@@ -7,19 +7,23 @@ public class FitnessCheckActiveTask extends ActiveTask implements Serializable {
     protected int walkDuration;
     protected int restDuration;
     protected String intendedUse;
+    protected String generalOutputDirectory;
 
     /**
      * Returns an initialized fitness check active task using the specified identifier and array of steps.
      *
-     * @param identifier    The unique identifier for the task.
-     * @param walkDuration  The number of seconds for that the user should walk.
-     * @param restDuration  The number of seconds for that the user should walk.
+     * @param identifier                The unique identifier for the task.
+     * @param walkDuration              The number of seconds for that the user should walk.
+     * @param restDuration              The number of seconds for that the user should walk.
+     * @param generalOutputDirectory    Where the resulting json file should be saved
      */
-    public FitnessCheckActiveTask(String identifier, int walkDuration, int restDuration, String intendedUse) {
+    public FitnessCheckActiveTask(String identifier, int walkDuration, int restDuration, String intendedUse,
+                                  String generalOutputDirectory) {
         super(identifier);
         this.walkDuration = walkDuration;
         this.restDuration = restDuration;
         this.intendedUse = intendedUse;
+        this.generalOutputDirectory = generalOutputDirectory;
     }
 
     public int getWalkDuration() {
@@ -32,5 +36,9 @@ public class FitnessCheckActiveTask extends ActiveTask implements Serializable {
 
     public String getIntendedUse() {
         return intendedUse;
+    }
+
+    public String getGeneralOutputDirectory() {
+        return generalOutputDirectory;
     }
 }
